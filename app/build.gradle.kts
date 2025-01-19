7190f2dde83caec6812cfb58e6f9e5580a821286
Gourav Khunger <gouravkhunger18@gmail.com>
2025-01-19 18:07:26 +0530

    chore: add leakcanary deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,6 +143,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.leakcanary)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
