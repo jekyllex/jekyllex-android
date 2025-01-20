5d6118f12d2c663b1b971589e7228d6bf7c151ac
Gourav Khunger <gouravkhunger18@gmail.com>
2025-01-20 21:03:48 +0530

    chore: add preference datastore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,6 +126,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
