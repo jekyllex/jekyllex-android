8e7299342afbf9513aad5772bb0a33f5940241fc
Gourav Khunger <gouravkhunger18@gmail.com>
2025-03-24 22:00:42 +0530

    chore: foss ready (#18)
    * chore: remove non foss tooling
    
    * chore(ci): remove redundant build step
    
    * chore: remove unused strings
    
    * chore: EOF new line
    
    * chore: remove gsm
    
    * chore: remove gsm

--- app/src/main/java/xyz/jekyllex/ui/activities/settings/SettingsActivity.kt
+++ app/src/main/java/xyz/jekyllex/ui/activities/settings/SettingsActivity.kt
@@ -54,7 +54,6 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import com.google.firebase.analytics.FirebaseAnalytics
 import me.zhanghai.compose.preference.ProvidePreferenceLocals
 import me.zhanghai.compose.preference.footerPreference
 import me.zhanghai.compose.preference.preference
@@ -79,13 +78,9 @@ import xyz.jekyllex.utils.Constants.LICENSES
 import xyz.jekyllex.utils.Constants.ISSUES_URL
 import xyz.jekyllex.utils.Constants.PAT_SETTINGS_URL
 
-private lateinit var firebaseAnalytics: FirebaseAnalytics
-
 class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         setContent {
             JekyllExTheme {
@@ -359,25 +354,6 @@ fun SettingsView() {
                         Text(context.getString(R.string.jekyll_flags_summary))
                         it.takeIf { it.isNotBlank() }?.let { flag -> Text(flag) }
                     },
-                )
-
-                preferenceCategory(
-                    key = "telemetry",
-                    title = { Text("Telemetry") }
-                )
-
-                switchPreference(
-                    key = CRASH_REPORTS.key,
-                    defaultValue = CRASH_REPORTS.defaultValue.get(),
-                    title = { Text(context.getString(R.string.crash_reports_title)) },
-                    summary = { Text(context.getString(R.string.crash_reports_summary)) },
-                )
-
-                switchPreference(
-                    key = LOG_ANALYTICS.key,
-                    defaultValue = LOG_ANALYTICS.defaultValue.get(),
-                    title = { Text(context.getString(R.string.analytics_title)) },
-                    summary = { Text(context.getString(R.string.analytics_summary)) },
                 )
 
                 preferenceCategory(
