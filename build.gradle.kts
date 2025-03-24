0b8ccdd1cba608b244c5d45f8f36a43844779c0a
Gourav Khunger <gouravkhunger18@gmail.com>
2025-03-24 21:38:36 +0530

    chore: remove gsm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
