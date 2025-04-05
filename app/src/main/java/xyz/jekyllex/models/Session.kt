8b3a4c20d85c7c4f49cba369f0172238c8b6bb13
Gourav Khunger <gouravkhunger18@gmail.com>
2025-04-05 22:48:28 +0530

    feat: implement the session modal

--- app/src/main/java/xyz/jekyllex/models/Session.kt
+++ app/src/main/java/xyz/jekyllex/models/Session.kt
@@ -25,41 +25,142 @@
 package xyz.jekyllex.models
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import java.io.BufferedReader
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import xyz.jekyllex.utils.Constants.BIN_DIR
+import xyz.jekyllex.utils.Constants.HOME_DIR
+import xyz.jekyllex.utils.NativeUtils.buildEnvironment
+import xyz.jekyllex.utils.drop
+import xyz.jekyllex.utils.formatDir
+import xyz.jekyllex.utils.isDenied
+import java.io.File
 
 private const val LOG_TAG = "Session"
 
-data class Session(val process: Process, var isRunning: Boolean = true) {
-    private val _logs: MutableList<String> = mutableListOf()
-    val logs
-        get() = _logs.toTypedArray()
+class Session(val notificationCallback: () -> Unit = {}) {
+    private var trimLogs = false
+    private lateinit var process: Process
+    private val processBuilder = ProcessBuilder()
+    private var inputReader: BufferedReader? = null
+    private var errorReader: BufferedReader? = null
 
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                process.inputStream.bufferedReader().forEachLine {
-                    _logs.add(it)
-                }
-            } catch (e: Exception) {
-                Log.d(LOG_TAG, "Exception while reading output: $e")
-            }
+    private var _runningCommand = ""
+    private var _isRunning = mutableStateOf(false)
+    private val _logs = MutableStateFlow(listOf<String>())
+    val logs get() = _logs
+
+    val isRunning
+        get() = _isRunning.value
+    val runningCommand
+        get() = _runningCommand
+
+    fun appendLog(log: String) {
+        _logs.value += log
+    }
+
+    fun clearLogs() {
+        _logs.value = listOf()
+    }
+
+    fun setLogTrimming(shouldTrim: Boolean) {
+        trimLogs = shouldTrim
+    }
+
+    fun exec(
+        command: Array<String>,
+        dir: String = HOME_DIR,
+        env: Array<String> = buildEnvironment(dir),
+        callBack: () -> Unit = {}
+    ) {
+        if (_isRunning.value) return
+
+        appendLog("${dir.formatDir("/")} $ ${command.joinToString(" ")}")
+
+        if (command.isDenied()) {
+            appendLog("Command not allowed!")
+            return
         }
 
         CoroutineScope(Dispatchers.IO).launch {
+            _isRunning.value = true
+            _runningCommand = command.joinToString(" ")
+            notificationCallback()
+
             try {
-                process.errorStream.bufferedReader().forEachLine {
-                    _logs.add(it)
+                val environment = processBuilder.environment()
+                env.forEach {
+                    val envVar = it.split("=")
+                    if (envVar.size != 2) return@forEach
+                    environment[envVar[0]] = envVar[1]
                 }
+
+                process = processBuilder
+                    .command(
+                        if (command[0].contains("/bin")) command.toMutableList()
+                        else mutableListOf("$BIN_DIR/${command.getOrNull(0)}", *command.drop(1))
+                    )
+                    .directory(File(dir))
+                    .redirectErrorStream(true)
+                    .start()
+
+                inputReader = process.inputStream.bufferedReader()
+                errorReader = process.errorStream.bufferedReader()
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        inputReader?.forEachLine {
+                            appendLog(it)
+                        }
+                    } catch (e: Exception) {
+                        Log.d(LOG_TAG, "Exception while reading output: $e")
+                    }
+                }
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        errorReader?.forEachLine {
+                            appendLog(it)
+                        }
+                    } catch (e: Exception) {
+                        Log.d(LOG_TAG, "Exception while reading error: $e")
+                    }
+                }
+
+                val exitCode = process.waitFor()
+                if (exitCode != 0) appendLog("Process exited with code $exitCode")
+
+                processStopped()
+                callBack()
             } catch (e: Exception) {
-                Log.d(LOG_TAG, "Exception while reading error: $e")
+                processStopped()
+                appendLog("${e.cause}")
+                if (::process.isInitialized) process.destroy()
+                Log.e(LOG_TAG, "Error while starting process: $e")
             }
         }
     }
 
-    fun kill() {
+    private fun processStopped() {
+        _runningCommand = ""
+        _isRunning.value = false
+        if (trimLogs) _logs.value = _logs.value.takeLast(200)
+        notificationCallback()
+    }
+
+    fun killProcess() {
+        if (!_isRunning.value) return
+        if (::process.isInitialized) process.destroy()
+    }
+
+    fun killSelf() {
+        clearLogs()
+        errorReader?.close()
+        inputReader?.close()
         process.destroy()
-        isRunning = false
+        _isRunning.value = false
     }
 }
