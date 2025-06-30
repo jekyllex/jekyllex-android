22557c8778a6f0caaec463f1c1c7723da29e7397
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-01 00:37:08 +0530

    feat: release v0.2.0 (#24)
    * chore: add changelog for v0.1.0
    
    * chore: bump versionCode and versionName
    
    * chore: add changelog for v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "xyz.jekyllex"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "v0.1.0"
+        versionCode = 2
+        versionName = "v0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
