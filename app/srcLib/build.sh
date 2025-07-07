fd186006b48cf3d819ac1a16106b3d5d710b364b
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-07 23:09:51 +0530

    feat: add a bootstrap build script

---
+++ app/srcLib/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# Setup environment
+mv termux-packages bootstrap
+cd bootstrap
+rm termux-packages/scripts/properties.sh termux-packages/scripts/build-bootstraps.sh
+mv termux-packages/* .
+rm -rf termux-packages
+mv properties.sh run-docker.sh build-bootstraps.sh scripts/
+for package in patches/*; do cp -r "$package" packages; done
+find packages/git -type f -name "*subpackage*" -exec rm {} +
+find packages/libxml2 -type f -name "*python*" -exec rm {} +
+
+# Build and store bootstraps for each architecture
+for target_arch in aarch64 arm i686 x86_64; do
+  ./scripts/run-docker.sh ./clean.sh
+  ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --android10 --architectures "$target_arch"
+done
+
+# Store bootstrap
+mv *.zip ../../../bootstraps
