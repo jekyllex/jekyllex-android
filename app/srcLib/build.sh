256907182820f99810f393cdadad9929959e5e04
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-08 00:39:50 +0530

    chore(scrLib/build.sh): attempt parallel build for bootstraps

--- app/srcLib/build.sh
+++ app/srcLib/build.sh
@@ -11,11 +11,16 @@ for package in patches/*; do cp -r "$package" packages; done
 find packages/git -type f -name "*subpackage*" -exec rm {} +
 find packages/libxml2 -type f -name "*python*" -exec rm {} +
 
-# Build and store bootstraps for each architecture
+# Build bootstraps for each architecture in parallel
 for target_arch in aarch64 arm i686 x86_64; do
-  ./scripts/run-docker.sh ./clean.sh
-  ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --android10 --architectures "$target_arch"
+  (
+    ./scripts/run-docker.sh ./clean.sh
+    ./scripts/run-docker.sh ./scripts/build-bootstraps.sh --android10 --architectures "$target_arch"
+  ) &
 done
+
+# Wait for all background builds to finish
+wait
 
 # Store bootstrap
 mv *.zip ../../../bootstraps
