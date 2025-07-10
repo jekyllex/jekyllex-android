a1bcdafedcfda24abbac9fe0b61ecf62405376d8
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-10 16:14:21 +0530

    fix: mv order

--- app/srcLib/build.sh
+++ app/srcLib/build.sh
@@ -6,8 +6,8 @@ sudo mkdir -p /data /home/builder
 sudo chown $USER:$USER /data /home/builder
 cp -r bootstrap/* termux-packages /home/builder/
 cd /home/builder
+mv *.sh termux-packages/scripts/
 mv termux-packages/* .
-mv *.sh scripts/
 for package in patches/*; do cp -r "$package" packages; done
 find packages/git -type f -name "*subpackage*" -exec rm {} +
 find packages/libxml2 -type f -name "*python*" -exec rm {} +
@@ -15,13 +15,6 @@ find packages/libxml2 -type f -name "*python*" -exec rm {} +
 if [[ -z "$ANDROID_HOME" || -z "$NDK" ]]; then
   ./scripts/setup-android-sdk.sh
 fi
-
-# debug
-echo "Current directory: $(pwd)"
-echo "Contents of current directory:"
-ls -la
-echo "Contents of /home/builder:"
-ls -la /home/builder
 
 # Build bootstraps for each architecture
 ./scripts/build-bootstraps.sh --android10 --architectures aarch64
