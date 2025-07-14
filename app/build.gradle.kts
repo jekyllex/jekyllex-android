b51207fda90c87322cae70326334683f8398777e
Gourav Khunger <gouravkhunger18@gmail.com>
2025-07-14 16:51:56 +0530

    feat: release v0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "xyz.jekyllex"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "v0.2.1"
+        versionCode = 4
+        versionName = "v0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
